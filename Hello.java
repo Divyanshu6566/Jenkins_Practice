14a4a762b00837fbd7b7ded976db23050e4fb3b8
Divyanshu6566 <divyanshu2003mth@gmail.com>
2024-12-27 10:47:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println(new Date());
-
+        System.out.println("Divyanshu");
         Student student = new Student();
         student.sayHello();
     }
